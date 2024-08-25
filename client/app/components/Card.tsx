f7f31c846277be42e5aed71871960814f86ab9e7
Ben Ohayon <ben1679@gmail.com>
2024-08-25 17:03:55 +0300

    Created starting page

---
+++ client/app/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Parent } from '../lib/propTypes'
+
+export default function Card({
+    children
+}: Parent) {
+    return (
+        <div className='bg-white p-3 rounded-md shadow-md'>
+            {children}
+        </div>
+    )
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,35 @@
 'use client'
-import { useSession } from "next-auth/react";
-import { GoogleSignInButton } from "./components/authButtons";
 
-
+import { signOut, useSession } from "next-auth/react";
+import { EmailSignInButton, GoogleSignInButton } from "./components/authButtons";
+import Card from "./components/Card";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const {data: session} = useSession()
-  if(session && session.user) {
-    console.log(session.user);
-    
-    return <div>authed...</div>
-  }
+	const { data: session } = useSession()
+	const router = useRouter()
+
+	if (session && session.user) {
+		console.log(session)
+		// router.push('/home')
+	}
 
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <GoogleSignInButton/>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-300">
+			<Card>
+				<div className="flex flex-col justify-between">
+					<div className="w-[500px] h-[600px] flex flex-col gap-6">
+						<div className="text-[20px] text-center font-semibold">HuddleHub</div>
+						<div className="flex flex-col gap-4">
+							<GoogleSignInButton />
+							<EmailSignInButton />
+						</div>
+						{(session && session.user) && <div onClick={() => signOut()} className="text-[20px] text-center font-semibold cursor-pointer">Sign Out</div>}
+					</div>
+					<div className="text-[12px] text-center">Powered by HuddleHub</div>
+				</div>
+			</Card>
+		</main>
+	);
 }
