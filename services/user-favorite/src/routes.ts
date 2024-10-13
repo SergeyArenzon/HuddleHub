b02c3a64efcd2de3aacc3c73d58508703ccbd4f4
SergeyArenzon <arenzonsergey@gmail.com>
2024-10-13 20:13:08 +0300

    remove srvs

--- services/user-favorite/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { tokenHandler } from "./controllers";
-
-
-const userRoutes = async(server: FastifyInstance) =>  {
-    server.post('/token', tokenHandler);
-};
-
-export default userRoutes;
