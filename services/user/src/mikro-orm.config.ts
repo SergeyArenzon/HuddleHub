17113cc8366fe1cfe757e397b2c6c33025c999b9
SergeyArenzon <arenzonsergey@gmail.com>
2024-12-01 14:59:22 +0200

    connect user to user-db

--- services/user/src/mikro-orm.config.ts
+++ services/user/src/mikro-orm.config.ts
@@ -2,15 +2,10 @@ import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 const config: MikroOrmModuleSyncOptions = {
+  clientUrl: process.env.USER_DB_HOST, // Connection URL
   entities: ['./dist/entities'], // Path to compiled entities
   entitiesTs: ['./src/entities'], // Path to TypeScript entities
-  dbName: 'mydb',
   driver: PostgreSqlDriver, // Specify PostgreSQL driver
-  user: 'myuser',
-  password: 'mypassword',
-  host: 'localhost',
-  port: 5432,
   debug: true, // Enable for development
 };
-
 export default config;
--- services/user/src/user.module.ts
+++ services/user/src/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-// import mikroOrmConfig from './mikro-orm.config';
-// import { MikroOrmModule } from '@mikro-orm/nestjs';
-// import { User } from './entities/user.entity';
+import mikroOrmConfig from './mikro-orm.config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
-    // MikroOrmModule.forRoot(mikroOrmConfig),
-    // MikroOrmModule.forFeature({ entities: [User] }),
+    MikroOrmModule.forRoot(mikroOrmConfig),
+    MikroOrmModule.forFeature({ entities: [User] }),
   ],
   controllers: [UserController],
 })
