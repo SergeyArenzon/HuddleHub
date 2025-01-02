cda4efbd9c0fc52d251bac2eff4e976e32df6eea
SergeyArenzon <arenzonsergey@gmail.com>
2025-01-02 21:35:40 +0200

    add react 19

---
+++ client-1/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import Google from "next-auth/providers/google"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+})
---
+++ client-1/src/app/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SessionProvider } from 'next-auth/react';
+
+// Create a client
+const queryClient = new QueryClient();
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export function Providers({ children }: Props) {
+  return (
+    // <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    // </SessionProvider>
+  );
+}
