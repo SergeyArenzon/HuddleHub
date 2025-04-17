a0e5c00218a9d85309250bffe23641b1225a03a0
SergeyArenzon <sergeyr1991@gmail.com>
2025-04-17 10:01:47 +0300

    init shadcn

--- client-nuxt/nuxt.config.ts
+++ client-nuxt/nuxt.config.ts
@@ -3,6 +3,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  modules: [
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
--- client-nuxt/pages/index.vue
+++ client-nuxt/pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <h1>Welcome to HuddleHub</h1>
     <p>Your custom application content goes here</p>
+    <Button>Click me</Button>
   </div>
 </template> 
