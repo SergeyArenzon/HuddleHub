b4142a985ce111de7b917a99cb84563e31d00cfe
SergeyArenzon <sergeyr1991@gmail.com>
2025-05-12 09:16:14 +0300

    add tour-system doc

---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import TourForm from '@/components/TourForm'
+
+
+export default function Test() {
+
+  return (
+    <div className="flex items-center justify-center col-start-1 col-span-1 row-start-2 row-span-1">
+        <TourForm />
+
+    </div>
+  );
+}
